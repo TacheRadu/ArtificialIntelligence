d7f402233e7a3b981ebf45132dee256b70d01721
HristeaIonut <hristeaionut72@gmail.com>
2021-10-13 08:42:15 +0300

    much backtracking such amazing

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,3 +1,7 @@
+import random
+import time
+from random import randint
+
 global n, representation
 
 
@@ -12,7 +16,6 @@ def should_stop(representation):
 
 
 def validate(state, husband, wife, who_gets_back):
-
     # who_gets_back == 0 -> husband gets back
     if representation[0][husband] != representation[1][wife]:
         return
@@ -37,20 +40,37 @@ def validate(state, husband, wife, who_gets_back):
 def transition(state: list, husband, wife, who_gets_back):
     global representation
     result = validate(state, husband, wife, who_gets_back)
-    if not result:
-        print("Invalid transition!")
-    else:
+    if result:
+        print(result)
         representation = result
+
+
+def permute(size, lista):
+    if size == 1:
+        return lista
+    else:
+        return [
+            y + x
+            for y in permute(1, lista)
+            for x in permute(size - 1, lista)
+        ]
+
+
+def run():
+    global representation
+    permutations = permute(2, list(map(str, range(n))))
+    position = 0
+
+    while not should_stop(representation):
+        transition(representation, int(permutations[position][0]), int(permutations[position][1]), randint(0,1))
+        position = (position+1) % len(permutations)
 
 
 def main():
     global n, representation
     n = int(input('Number of pairs:'))
     representation = initialize(n)
-    transition(representation, 1, 1, 0)
-    print(representation)
-    transition(representation, 2, 2, 1)
-    print(representation)
+    run()
 
 
 if __name__ == '__main__':
