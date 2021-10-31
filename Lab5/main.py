e9e63b967c0b6880f31ae494470dbe02c429d86d
HristeaIonut <hristeaionut72@gmail.com>
2021-10-31 14:56:52 +0200

    ionut mastermind

--- Lab5/main.py
+++ Lab5/main.py
@@ -16,5 +16,70 @@ def generate_colors(n, k, m):
     return colors[:k]
 
 
-initial_state = init()
-print(generate_colors(initial_state["n"], initial_state["k"], initial_state["m"]))
+def check_sequence(sequence, guess):
+    correct_positions = 0
+    for i in range(len(sequence)):
+        if sequence[i] == guess[i]:
+            correct_positions += 1
+    
+    correct_colors = 0
+    for i in guess:
+        if i in sequence:
+            correct_colors += 1
+    return correct_positions, correct_colors
+
+
+# guess = list(map(int, input("Input your guess: ").split()))
+
+
+def main():
+    mode = input("Input the mode(1 for auto, 2 for manual): ")
+    initial_state = init()
+    if mode == "1":
+        sequence = generate_colors(initial_state["n"], initial_state["k"], initial_state["m"])
+    else:
+        sequence = list(map(int, input("Input the sequence: ").split()))
+        
+    if len(sequence) != initial_state["k"]:
+        print("Wrong sequence length")
+        return
+        
+    for elem in sequence:
+        if sequence.count(elem) > initial_state["m"]:
+            print("Too many duplicates")
+            return
+            
+    for elem in sequence:
+        if elem < 0 or elem >= initial_state["n"]:
+            print("Wrong color")
+            return
+        
+    print(sequence)
+        
+    for i in range(initial_state["k"] * 2):
+        guess = list(map(int, input("Input your guess: ").split()))
+        if len(guess) != initial_state["k"]:
+            print("Wrong sequence length")
+            return
+            
+        for elem in guess:
+            if guess.count(elem) > initial_state["m"]:
+                print("Too many duplicates")
+                return
+            
+        for elem in guess:
+            if elem < 0 or elem >= initial_state["n"]:
+                print("Wrong color")
+                return
+            
+        correct_positions, correct_colors = check_sequence(sequence, guess)
+        print("Correct positions: {}, correct colors: {}".format(correct_positions, correct_colors))
+        initial_state["corrects"] += correct_positions
+        initial_state["prev_states"].append({"corrects": initial_state["corrects"], "guess": guess})
+        if correct_positions == initial_state["k"]:
+            print("You won!")
+            return
+            
+if __name__ == "__main__":
+    main()
+    
