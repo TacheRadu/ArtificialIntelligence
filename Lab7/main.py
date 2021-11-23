0a9f1982fc017a8a5043fe43318f61e18b13c88d
Radu Tache <radutache76@gmail.com>
2021-11-23 18:09:08 +0200

    Now it learns

--- Lab7/main.py
+++ Lab7/main.py
@@ -1,24 +1,25 @@
-from os import X_OK
 import numpy as np
 from numpy.core.fromnumeric import argmax
 
 EPOCS = int(input("How many epochs? "))
 LR = float(input("Learning rate? "))
-HIDDEN_LAYER_SIZE = 10
+HIDDEN_LAYER_SIZE = 69
 f = open("dataset_and.txt", "r")
-matrix = np.array(list(map(lambda x: int(x), f.read().split()))).reshape(4,3)
-labels = matrix[:,2]
-matrix = matrix[:,:2]
+matrix = np.array(list(map(lambda x: int(x), f.read().split()))).reshape(4, 3)
+labels = matrix[:, 2]
+matrix = matrix[:, :2]
 
-def sigmod(x):
+
+def sigmoid(x):
     return 1/(1+np.exp(-x))
+
 
 def test(w_input, w_hidden, bias_input, bias_hidden):
     count = 0
     for j in range(4):
         x = matrix[j]
-        z = sigmod(np.dot(w_input, x) + bias_input)
-        y = sigmod(np.dot(w_hidden, z) + bias_hidden)
+        z = sigmoid(np.dot(w_input, x) + bias_input)
+        y = sigmoid(np.dot(w_hidden, z) + bias_hidden)
         if argmax(y) == labels[j]:
             count += 1
     
@@ -26,22 +27,24 @@ def test(w_input, w_hidden, bias_input, bias_hidden):
 
 
 def train(matrix, epochs, learning_rate):
-    w_input = np.random.normal(0, 1/2**0.5, (HIDDEN_LAYER_SIZE, 2))
+    w_input = np.random.normal(0, 1/2 ** 0.5, (HIDDEN_LAYER_SIZE, 2))
+    #w_input = np.ones((HIDDEN_LAYER_SIZE, 2))
     bias_input = np.zeros(HIDDEN_LAYER_SIZE)
-    w_hidden = np.random.normal(0, 1/HIDDEN_LAYER_SIZE**0.5, (2, HIDDEN_LAYER_SIZE))
+    w_hidden = np.random.normal(0, 1/HIDDEN_LAYER_SIZE ** 0.5, (2, HIDDEN_LAYER_SIZE))
+    #w_hidden = np.ones((2, HIDDEN_LAYER_SIZE))
     bias_hidden = np.zeros(2)
     best = 0
     for i in range(epochs):
         for j in range(4):
             x = matrix[j]
-            z = sigmod(np.dot(w_input, x) + bias_input)
-            y = sigmod(np.dot(w_hidden, z) + bias_hidden)
-            
-            error_output = (labels[j] - y) * y * (1 - y)
-            error_hidden =  z * (1 - z) * np.dot(error_output, w_hidden)
+            z = sigmoid(np.dot(w_input, x) + bias_input)
+            y = sigmoid(np.dot(w_hidden, z) + bias_hidden)
+
+            error_output = (y - [int(x == labels[j]) for x in range(2)]) * y * (1 - y)
+            error_hidden = z * (1 - z) * np.dot(w_hidden.T, error_output)
 
             w_hidden -= learning_rate * np.outer(error_output.T, z)
-            w_input -= learning_rate * np.outer(error_hidden, x)
+            w_input -= learning_rate * np.outer(error_hidden.T, x)
             bias_hidden -= learning_rate * error_output
             bias_input -= learning_rate * error_hidden
         
@@ -51,5 +54,6 @@ def train(matrix, epochs, learning_rate):
     
     return w_input, w_hidden, bias_input, bias_hidden
 
-train(matrix, EPOCS, LR)
 
+if __name__ == '__main__':
+    train(matrix, EPOCS, LR)
